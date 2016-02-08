65274ae95330d47faf0c38cf4921d4be6fb87058
Kilian Stein <kstein@cafat.nc>
2016-02-08 14:23:04 +1100

    reorganisation des ressources

--- src/main/java/ki/optisoins/OptiSoinsConfiguration.java
+++ src/main/java/ki/optisoins/OptiSoinsConfiguration.java
@@ -1,9 +1,9 @@
 package ki.optisoins;
 
 public class OptiSoinsConfiguration {
-  public static String jasperReportFeuilleSoinsTemplateUrl = "FeuilleSoinsAuxiliaireMedicale.jrxml";
-  public static String jasperReportEtat100TemplateUrl = "Etat_100.jrxml";
-  public static String jasperReportEtatAideMedicalSudTemplateUrl = "Etat_AideMedical_Sud.jrxml";
+  public static String jasperReportFeuilleSoinsTemplateUrl = "jasper/feuillesoins/FeuilleSoinsAuxiliaireMedicale.jrxml";
+  public static String jasperReportEtat100TemplateUrl = "jasper/etat_100/Etat_100.jrxml";
+  public static String jasperReportEtatAideMedicalSudTemplateUrl = "jasper/etat_aidemedicale_sud/Etat_AideMedicale_Sud.jrxml";
   public static String outputDirectory = "fichiersGeneres";
   public static String dataDirectory = "donnees";
   public static String dataDefault = "donnees.xls";
